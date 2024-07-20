cc7062f7d751ca38b6fbc494b7e5b706234cca1a
igorskyflyer <igor.dvlpr@gmail.com>
2024-07-20 19:13:10 +0200

    Major refactor

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,76 +1,80 @@
 // Author: Igor Dimitrijević (@igorskyflyer)
 
-type CombinatorChild<Input extends string> =
-  Input extends `${infer _Parent}> ${infer Child}`
-    ? Child extends keyof HTMLElementTagNameMap
-      ? HTMLElementTagNameMap[Child]
-      : HTMLElement
-    : Input extends `${infer _Parent}>${infer Child}`
-      ? Child extends keyof HTMLElementTagNameMap
-        ? HTMLElementTagNameMap[Child]
-        : HTMLElement
-      : never
-
-type CombinatorDescendant<Input extends string> =
-  Input extends `${infer _FirstSelector} ${infer SecondSelector}`
-    ? SecondSelector extends keyof HTMLElementTagNameMap
-      ? HTMLElementTagNameMap[SecondSelector]
-      : HTMLElement
-    : never
-
-type SelectorTagClass<Input extends string> =
-  Input extends `${infer Tag}.${infer _ClassName}`
-    ? Tag extends keyof HTMLElementTagNameMap
-      ? HTMLElementTagNameMap[Tag]
-      : HTMLElement
-    : never
-
-type SelectorTagId<Input extends string> =
-  Input extends `${infer Tag}#${infer _Id}`
-    ? Tag extends keyof HTMLElementTagNameMap
-      ? HTMLElementTagNameMap[Tag]
-      : HTMLElement
-    : never
-
-type SelectorTagAttribute<Input extends string> =
-  Input extends `${infer Tag}[${infer _Attribute}]`
-    ? Tag extends keyof HTMLElementTagNameMap
-      ? HTMLElementTagNameMap[Tag]
-      : HTMLElement
-    : never
-
-type CombinatorNextSiblingTag<Input extends string> =
-  Input extends `${infer _First}+ ${infer Tag}`
-    ? Tag extends keyof HTMLElementTagNameMap
-      ? HTMLElementTagNameMap[Tag]
-      : HTMLElement
-    : Input extends `${infer _First}+${infer Tag}`
-      ? Tag extends keyof HTMLElementTagNameMap
-        ? HTMLElementTagNameMap[Tag]
-        : HTMLElement
-      : never
+import type { Trim } from '@igor.dvlpr/common-types'
 
 // TODO: implement all selectors (???)
-// General Sibling Combinator
-// Syntax: element ~ sibling
-// Example: h1 ~ p
 // https://www.w3.org/TR/selectors-4/
-type QuerySelector<Input extends string> =
+
+// Utitlity types
+type TagNameToHTMLElement<TagName extends string> =
+  TagName extends keyof HTMLElementTagNameMap
+    ? HTMLElementTagNameMap[TagName]
+    : HTMLElement
+
+type InferTagName<Input extends string> = Input extends string
+  ? TagNameToHTMLElement<Trim<Input>>
+  : never
+
+// Pseudo classes
+type PseudoRoot<Input extends string> = Input extends string
+  ? Trim<Input> extends ':root'
+    ? HTMLElementTagNameMap['html']
+    : never
+  : never
+
+// Selectors
+type DoSelector<
+  Input extends string,
+  Selector extends string
+> = Trim<Input> extends `${infer Tag}${Selector}${infer _Rest}`
+  ? InferTagName<Tag>
+  : never
+
+type SelectorTagClass<Input extends string> = DoSelector<Input, '.'>
+type SelectorTagId<Input extends string> = DoSelector<Input, '#'>
+
+type SelectorTagAttribute<Input extends string> =
+  Input extends `${infer Tag}[${infer _Attribute}]` ? InferTagName<Tag> : never
+
+// Combinators
+type DoCombinator<
+  Input extends string,
+  Selector extends string
+> = Trim<Input> extends `${infer _Ignored}${Selector}${infer Element}`
+  ? InferTagName<Element>
+  : never
+
+type CombinatorDescendant<Input extends string> = DoCombinator<Input, ' '>
+type CombinatorChild<Input extends string> = DoCombinator<Input, '>'>
+type CombinatorNextSiblingTag<Input extends string> = DoCombinator<Input, '+'>
+type CombinatorSubsequentSiblingTag<Input extends string> = DoCombinator<
+  Input,
+  '~'
+>
+type CombinatorColumnTag<Input extends string> = DoCombinator<Input, '||'>
+
+export type QuerySelector<Input extends string> =
   Input extends keyof HTMLElementTagNameMap
     ? HTMLElementTagNameMap[Input]
-    : CombinatorChild<Input> extends never
-      ? CombinatorNextSiblingTag<Input> extends never
-        ? CombinatorDescendant<Input> extends never
-          ? SelectorTagClass<Input> extends never
-            ? SelectorTagId<Input> extends never
-              ? SelectorTagAttribute<Input> extends never
-                ? HTMLElement
-                : SelectorTagAttribute<Input>
-              : SelectorTagId<Input>
-            : SelectorTagClass<Input>
-          : CombinatorDescendant<Input>
-        : CombinatorNextSiblingTag<Input>
-      : CombinatorChild<Input>
+    : PseudoRoot<Input> extends string
+      ? CombinatorChild<Input> extends string
+        ? SelectorTagClass<Input> extends string
+          ? SelectorTagId<Input> extends string
+            ? SelectorTagAttribute<Input> extends string
+              ? CombinatorSubsequentSiblingTag<Input> extends string
+                ? CombinatorNextSiblingTag<Input> extends string
+                  ? CombinatorColumnTag<Input> extends string
+                    ? CombinatorDescendant<Input> extends string
+                      ? HTMLElement
+                      : CombinatorDescendant<Input>
+                    : CombinatorColumnTag<Input>
+                  : CombinatorNextSiblingTag<Input>
+                : CombinatorSubsequentSiblingTag<Input>
+              : SelectorTagAttribute<Input>
+            : SelectorTagId<Input>
+          : SelectorTagClass<Input>
+        : CombinatorChild<Input>
+      : PseudoRoot<Input>
 
 declare global {
   interface Document {
@@ -87,5 +91,3 @@ declare global {
     ): NodeListOf<QuerySelector<K>>
   }
 }
-
-export {}
